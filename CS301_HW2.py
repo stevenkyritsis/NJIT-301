4163c5ed3f1cdb8eb3b7a9b58805a3ad4c2833a2
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-10-01 15:35:16 -0400

    Update CS301_HW2.py

--- CS301_HW2.py
+++ CS301_HW2.py
@@ -2,5 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+#1
 sns.distplot(np.random.poisson(100, 10), kde = False)
 plt.show()
+
+#2
+def gradient_descent(x,y,theta,learning_rate=0.01,iterations=100):
+    
