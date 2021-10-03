2af0a8f6113ce87303c10421c4515b68ff8264a1
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-10-03 15:03:49 -0400

    Update CS301_HW2.py

--- CS301_HW2.py
+++ CS301_HW2.py
@@ -7,28 +7,13 @@ import seaborn as sns
 sns.distplot(np.random.poisson(100, 10), kde = False)
 plt.show()
 '''
-from scipy.stats import poisson
-
-# importing poisson fro scipy
-from scipy.stats import poisson
-
-# importing numpy as np
-import numpy as np
-
-# importing matplotlib as plt
-import matplotlib.pyplot as plt
-
 
 # creating a numpy array for x-axis
 # using step size as 1
 x = np.arange(0, 100, 1)
 
 # poisson distribution data for y-axis
-y = poisson.pmf(x, mu=10, loc=0)
-
-
-# plotting the graph
-plt.plot(x, y)
+y = np.random.poisson(x, 10)
 
 # showing the graph
 plt.show()
