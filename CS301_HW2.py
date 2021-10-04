0cfecc778bbb9867397f5daacaf3adcd0ca1d497
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2021-10-03 23:25:29 -0400

    Update CS301_HW2.py

--- CS301_HW2.py
+++ CS301_HW2.py
@@ -1,21 +1,32 @@
 import numpy as np
 from matplotlib import pyplot as plt
-'''
-import seaborn as sns
 
 #1
-sns.distplot(np.random.poisson(100, 10), kde = False)
-plt.show()
-'''
-
 # poisson distribution data for y-axis
-s = np.random.poisson(10, 100)
+s = np.random.poisson(lam=10, size=100)
+
+plt.hist(s)
 
 # showing the graph
 plt.show()
 
-'''
-
 #2
-def gradient_descent(x,y,theta,learning_rate=0.01,iterations=100):
- '''
+def gradient_descent(x, y, theta, alpha, m, numIterations):
+    xTrans = x.transpose()
+    for i in range(0, numIterations):
+        hypothesis = np.dot(x, theta)
+        loss = hypothesis - y
+        # avg cost per example (the 2 in 2*m doesn't really matter here.
+        # But to be consistent with the gradient, I include it)
+        cost = np.sum(loss ** 2) / (2 * m)
+        print("Iteration %d | Cost: %f" % (i, cost))
+        # avg gradient per example
+        gradient = np.dot(xTrans, loss) / m
+        # update
+        theta = theta - alpha * gradient
+    return theta
+
+mu = np.mean(s)
+var = np.var(s, ddof=1)
+
+gradient_descent(gradient=10.0, start=100.0, learn_rate=0.2, n_iter=1)
